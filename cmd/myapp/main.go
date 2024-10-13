e561bd465a8b0785d1b77cbfb59fc40a66ed6593
Danilov_AS <bigtem@mail.ru>
2024-10-13 20:05:38 +0300

    init commit

---
+++ cmd/myapp/main.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Hello, world!")
+}
+
+func Greeter(name string) {
+	fmt.Println("Hello" + name + "!")
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/SensusX/myapp
+
+go 1.22.2
